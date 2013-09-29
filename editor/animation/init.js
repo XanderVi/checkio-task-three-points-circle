7487e7f70695db3a639921a7b4a250a9d0d200a2
Valentin Bryukhanov <bvv.mag@gmail.com>
2013-09-29 19:40:29 +1200

    animation

--- editor/animation/init.js
+++ editor/animation/init.js
@@ -108,10 +108,137 @@ requirejs(['ext_editor_1', 'jquery_190', 'raphael_210'],
         var colorGrey1 = "#EBEDED";
 
         var colorWhite = "#FFFFFF";
-        //Your Additional functions or objects inside scope
-        //
-        //
-        //
+
+        function createPath(x1, y1, x2, y2) {
+            return "M" + x1 + "," + y1 + "L" + x2 + "," + y2;
+        }
+
+        function ThreePointsCircle() {
+            var zx = 20;
+            var zy = 20;
+            var cellSize = 20;
+            var cellN = 10;
+            var fullSize = 2 * zx + cellSize * (cellN + 1);
+
+            var colorDark = "#294270";
+            var colorOrange = "#F0801A";
+            var colorBlue = "#6BA3CF";
+            var colorDarkBlue = "#0A82BD";
+            var colorWhite = "#FFFFFF";
+            var attrAxis = {"stroke": colorDark, "stroke-width": 2, "arrow-end": "classic"};
+            var attrCircle = {"stroke": colorDarkBlue, "stroke-width": 4};
+            var attrInnerLine = {"stroke": colorBlue, "stroke-width": 1, "stroke-dasharray": ["-"]};
+            var attrText = {"font-family": "Verdana", "font-size": 14, "stroke": colorDark};
+            var attrPoint = {"stroke": colorOrange, "fill": colorOrange, "r": cellSize / 4};
+
+            var delay = 400;
+
+            var paper;
+            var points;
+            var circle;
+            var activeRect;
+            var countP = 0;
+
+
+            this.createCanvas = function(dom){
+                paper = Raphael(dom, fullSize, fullSize, 0, 0);
+            };
+
+            this.createPlane = function(){
+                for (var i = 1; i <= cellN; i++) {
+                    paper.path(createPath(
+                        zx, fullSize - zy - i * cellSize,
+                        zx + cellN * cellSize + zx / 2, fullSize - zy - i * cellSize)
+                    ).attr(attrInnerLine);
+
+                    paper.path(createPath(
+                        zx + i * cellSize, fullSize - zy,
+                        zx + i * cellSize, fullSize - zy - cellN * cellSize - zy / 2)
+                    ).attr(attrInnerLine);
+                    paper.text(zx + i * cellSize, fullSize - zy / 2, String(i)).attr(attrText);
+                    paper.text(zx / 2, fullSize - zy - i * cellSize, String(i)).attr(attrText);
+                }
+
+                paper.path(createPath(zx, fullSize - zy / 2, zx, zy / 2)).attr(attrAxis);
+                paper.text(zx / 2, zy / 2, "Y").attr(attrText);
+                paper.path(createPath(zx / 2, fullSize - zy, fullSize - zx / 2, fullSize - zy)).attr(attrAxis);
+                paper.text(fullSize - zx / 2, fullSize - zy / 2, "X").attr(attrText);
+                paper.text(zx / 2, fullSize - zy / 2, "0").attr(attrText);
+            };
+
+            this.createPoints = function(pointString) {
+                points = paper.set();
+                var r = pointString.match(/\d+/gi);
+                for (var i = 0; i < 6; i += 2){
+                    var p = paper.circle(zx + r[i] * cellSize,
+                        fullSize - (zy + r[i + 1] * cellSize),
+                        1).attr(attrPoint);
+                    points.push(p);
+                }
+            };
+
+            this.createFeedback = function(this_e) {
+                if (!points) {
+                    points = paper.set();
+                }
+                activeRect = paper.rect(zx + cellSize / 2,
+                    fullSize - (zy + cellSize / 2 + cellSize * cellN),
+                    cellSize * cellN,
+                    cellSize * cellN
+                ).attr({"fill": colorWhite, "fill-opacity": 0, "opacity": 0});
+                activeRect.toFront();
+                activeRect.click(function(e){
+                    var clx = e.offsetX || e.layerX;
+                    var cly = e.offsetY || e.layerY;
+                    var x = Math.round((clx - zx) / cellSize);
+                    var y = Math.round((fullSize - cly - zy) / cellSize);
+                    var p = paper.circle(
+                        zx + x * cellSize,
+                        fullSize - (zy + y * cellSize),
+                        1).attr(attrPoint);
+                    p.coor = "(" + x + "," + y + ")";
+                    if (points[countP]) {
+                        points[countP].remove();
+                        points[countP] = p;
+                    }
+                    else {
+                        points.push(p)
+                    }
+                    countP = (countP + 1) % 3;
+                    if (points && points.length == 3) {
+                        var data = [];
+                        for (var i = 0; i < 3; i++) {
+                            data.push(points[i].coor);
+                        }
+                        this_e.sendToConsoleCheckiO(data.join(","));
+                        e.stopPropagation();
+                    }
+                });
+
+            };
+            this.createCircle = function(equation){
+                var r = /\(x([+-]\d+(?:\.\d+)?)\)\^2\+\(y([+-]\d+(?:\.\d+)?)\)\^2=(\d+(?:\.\d+)?)\^2/.exec(equation);
+                //var r = /\(x-(\d+)\)\^2+\(y-(\d+)\)\^2=(\d+\.\d+)\^2/.exec(equation);
+                var radius = r[3];
+                var a = -r[1];
+                var b = -r[2];
+                circle = paper.circle(zx + a * cellSize,
+                    fullSize - (zy + b * cellSize), 1).attr(attrCircle);
+
+                circle.animate({
+                    "r": radius * cellSize}, delay);
+                for (var i = 0; i < points.length; i++){
+                    points[i].toFront();
+                }
+            };
+
+            this.removeCircle = function() {
+                if (circle) {
+                    circle.remove();
+                }
+            }
+        }
+
 
 
     }
