9ad761d276f854e45a51def5a83a1ffe66ff300c
XanderVi <engineermexanic@gmail.com>
2018-07-04 16:47:42 +0300

    add JS

--- verification/referee.py
+++ verification/referee.py
@@ -5,4 +5,14 @@ from tests import TESTS
 
 api.add_listener(
     ON_CONNECT,
-    CheckiOReferee(tests=TESTS).on_ready)
+    CheckiOReferee(
+        tests=TESTS,
+        function_name={
+            "python": "circle",
+            "js": "circle"
+        },
+        cover_code={
+            #'python-3': cover_codes.unwrap_args,
+            #'js-node': cover_codes.js_unwrap_args
+        }
+    ).on_ready)
